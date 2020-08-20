76edeb283a9a65877d9a46364f8441fdf4714052
iogr <antgorbunov@gmail.com>
2020-08-20 21:56:33 +0300

    2 commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
+  # Валидация, которая проверяет, что поля email и username не пустые и не равны
+  # nil. Если не задан email и username, объект не будет сохранен в базу.
+  validates :email, :username, presence: true
 end
