8add04bde401cccf43f3a3438108c4beebd54691
iogr <antgorbunov@gmail.com>
2020-08-26 19:54:03 +0300

    Question, user changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
-class Question < ActiveRecord::Base
+class Question < ApplicationRecord
   # Эта команда добавляет связь с моделью User на уровне объектов, она же
   # добавляет метод .user к данному объекту.
   #
@@ -6,39 +6,39 @@ class Question < ActiveRecord::Base
   # поймут это как просьбу найти в базе объект класса User со значением id,
   # который равен question.user_id.
   belongs_to :user
-
   validates :text, presence: true, length: { maximum: 255 }
-  # Все коллбэки
 
-  # Вызываем специальный метод и в виде символа передаём имя метода (его пишем сами),
-  # который будет вызван перед каким-либо действием
-
-  before_validation :before_validation
-  after_validation :after_validation
-
-  before_save :before_save
-  after_save :after_save
-
-  before_create :before_create
-  after_create :after_create
-
-  before_update :before_update
-  after_update :after_update
-
-  before_destroy :before_destroy
-  after_destroy :after_destroy
-
-  private
-
-  # Этот код нужен только для демонстрации валидаций, потом мы его удалим.
-  # Код создаёт все возможные пары методов before_validation, after_validation, before_save и так далее.
-
-  %w(validation save create update destroy).each do |action|
-    %w(before after).each do |time|
-      define_method("#{time}_#{action}") do
-        puts "******> #{time} #{action}"
-      end
-    end
-  end
-end
+#   # Все коллбэки
+#
+#   # Вызываем специальный метод и в виде символа передаём имя метода (его пишем сами),
+#   # который будет вызван перед каким-либо действием
+#
+#   before_validation :before_validation
+#   after_validation :after_validation
+#
+#   before_save :before_save
+#   after_save :after_save
+#
+#   before_create :before_create
+#   after_create :after_create
+#
+#   before_update :before_update
+#   after_update :after_update
+#
+#   before_destroy :before_destroy
+#   after_destroy :after_destroy
+#
+#   private
+#
+#   # Этот код нужен только для демонстрации валидаций, потом мы его удалим.
+#   # Код создаёт все возможные пары методов before_validation, after_validation, before_save и так далее.
+#
+#   %w(validation save create update destroy).each do |action|
+#     %w(before after).each do |time|
+#       define_method("#{time}_#{action}") do
+#         puts "******> #{time} #{action}"
+#       end
+#     end
+#   end
+# end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,59 +3,49 @@ require 'openssl'
 class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
-  # Когда мы вызываем метод questions у экземпляра класса User, рельсы
-  # поймут это как просьбу найти в базе все объекты класса Questions со
-  # значением user_id раexitвный user.id.
+  email_valid_mask = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  username_valid_mask = /\A\w+\z/
+
+  attr_accessor :password
+
   has_many :questions
 
   # username validations
 
   before_validation :username_to_downcase
-  validates :username, length: { maximum: 40 }
-  validates :username, format: { with: /\A\w+\z/ }
+  validates :username,
+            length: { maximum: 40 },
+            format: { with: username_valid_mask }
 
   # email validations
 
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email,
+            :username,
+            presence: true,
+            uniqueness: true,
+            format: { with: email_valid_mask },
+            on: :create
 
-  attr_accessor :password
-
-  # валидация будет проходить только при создании нового юзера
   validates :password, presence: true, on: :create
-
-  # и поле подтверждения пароля
-  validates_confirmation_of :password
 
   before_save :encrypt_password
 
-  # Основной метод для аутентификации юзера (логина). Проверяет email и пароль,
-  # если пользователь с такой комбинацией есть в базе, возвращает этого
-  # пользователя. Если нет — возвращает nil.
   def self.authenticate(email, password)
-    # Сперва находим кандидата по email
+
     user = find_by(email: email)
 
-    # Если пользователь не найден, возвращает nil
     return nil unless user.present?
-
-    # Формируем хэш пароля из того, что передали в метод
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
 
-    # Обратите внимание: сравнивается password_hash, а оригинальный пароль так
-    # никогда и не сохраняется нигде. Если пароли совпали, возвращаем
-    # пользователя.
     return user if user.password_hash == hashed_password
-
-    # Иначе, возвращаем nil
     nil
   end
 
+  private
   def encrypt_password
     if password.present?
       # Создаем т.н. «соль» — случайная строка, усложняющая задачу хакерам по
