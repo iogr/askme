344f9a542ace4d7049e7ae029ea9808dc07899e9
iogr <antgorbunov@gmail.com>
2020-08-27 18:39:23 +0300

    Current fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ require 'openssl'
 class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
-  email_valid_mask = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  username_valid_mask = /\A\w+\z/
+  EMAIL_VALID_MASK = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  USERNAME_VALID_MASK = /\A\w+\z/
 
   attr_accessor :password
 
@@ -13,25 +13,26 @@ class User < ApplicationRecord
   # username validations
 
   before_validation :username_to_downcase
+  before_save :encrypt_password
+
   validates :username,
+            presence: true,
             length: { maximum: 40 },
-            format: { with: UsernameValidMask }
+            format: { with: USERNAME_VALID_MASK }
 
   # email validations
 
   validates :email,
-            :username,
             presence: true,
             uniqueness: true,
-            format: { with: email_valid_mask },
+            format: { with: EMAIL_VALID_MASK }
+
+  validates :password,
+            presence: true,
+            confirmation: true,
             on: :create
 
-  validates :password, presence: true, on: :create
-
-  before_save :encrypt_password
-
   def self.authenticate(email, password)
-
     user = find_by(email: email)
 
     return nil unless user.present?
@@ -45,33 +46,21 @@ class User < ApplicationRecord
     nil
   end
 
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
   private
+
   def encrypt_password
     if password.present?
-      # Создаем т.н. «соль» — случайная строка, усложняющая задачу хакерам по
-      # взлому пароля, даже если у них окажется наша БД.
-      #У каждого юзера своя «соль», это значит, что если подобрать перебором пароль
-      # одного юзера, нельзя разгадать, по какому принципу
-      # зашифрованы пароли остальных пользователей
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      # Создаем хэш пароля — длинная уникальная строка, из которой невозможно
-      # восстановить исходный пароль. Однако, если правильный пароль у нас есть,
-      # мы легко можем получить такую же строку и сравнить её с той, что в базе.
       self.password_hash = User.hash_to_string(
         OpenSSL::PKCS5.pbkdf2_hmac(
           password, password_salt, ITERATIONS, DIGEST.length, DIGEST
         )
       )
-
-      # Оба поля попадут в базу при сохранении (save).
     end
-  end
-
-  # Служебный метод, преобразующий бинарную строку в шестнадцатиричный формат,
-  # для удобства хранения.
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
   end
 
   def username_to_downcase
