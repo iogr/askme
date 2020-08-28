1d23894d459c1a12d8d0edb0b05bac63491d6f77
iogr <antgorbunov@gmail.com>
2020-08-28 20:56:39 +0300

    Current: Views refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  # validates :user
   validates :text, presence: true, length: { maximum: 255 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'show' => 'users#show'
+  root to: 'users#index'
+
+  resources :users
+  resources :questions
+
+  # get 'show' => 'users#show'
 end
