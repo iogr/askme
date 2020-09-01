a329c6ecf7f9128a62efc40e8a14ff3a5985e390
iogr <antgorbunov@gmail.com>
2020-09-01 21:47:42 +0300

    SessionsController, UserController->UsersController bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root 'users#index'
 
   resources :users, except: [:destroy]
-  resources :session, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
+  # resources :sessions
   resources :questions, except: [:show, :new, :index]
 
-  get 'sign_up' => 'user#new'
+  get 'sign_up' => 'users#new'
   get 'log_in' => 'sessions#new'
   get 'log_out' => 'sessions#destroy'
 
