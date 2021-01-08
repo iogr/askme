8765f354c5c1dea3c10cc74283c38977e44f9bad
iogr <antgorbunov@gmail.com>
2021-01-08 18:32:29 +0300

    js for heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-const webpack = require('webpack')
+const webpack = require('webpack');
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
         jQuery: 'jquery/src/jquery'
     })
-)
+);
 
 // const webpack = require('webpack')
 // environment.plugins.append('Provide',
@@ -17,4 +17,4 @@ environment.plugins.prepend('Provide',
 //     })
 // )
 
-module.exports = environment
+module.exports = environment;
